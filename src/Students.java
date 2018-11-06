ddc53b95a33adabbe426df1fdccb791ae723c886
Yuehann <zyh990424@gmail.com>
2018-11-06 11:00:12 +0800

    update Students class

--- src/Students.java
+++ src/Students.java
@@ -1,4 +1,82 @@
+import java.util.ArrayList;
+public class Students extends User{
+	
+	private String studentId;
+	private String studentName;
+	private char gender;
+	private String nationality;
+	private String school;
+	private Integer year;
+	private ArrayList<Integer> indexList;
+	
+/*-------------------------constructor---------------------------- */	
+// Creates a new Student without specified attributes
+	public Students() {
+		
+	}
+// Creates a new Student with studentId, studentName, gender, nationality, school, year, indexList passed in
+	public Students(String studentId, String studentName, char gender, String nationality, 
+			String school, Integer year, ArrayList<Integer> indexList) {
+		
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.gender = gender;
+		this.nationality = nationality;
+		this.school = school;
+		this.year = year;
+		this.indexList = indexList;
+	}
+/*-----------------------get&set attributes-------------------------- */	
+// Get and set studentId
+	public String getStudenId() {
+		return this.studentId;
+	}
+	
+	public void setStudenId(String studentId) {
+		this.studentId = studentId;
+	}
+// Get and set studentName
+	public String getStudentName() {
+		return this.studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+// Get and set gender
+	public char getGender() {
+		return this.gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+// Get and set nationality
+	public String getNationality() {
+		return this.nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+// Get and set school
+	public String getSchool() {
+		return this.school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+// Get and set year
+	public Integer getYear() {
+		return this.year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+// Get and set indexList, a list of course index that student registered in
+	public ArrayList<Integer> getIndexList(){
+		return this.indexList;
+	}
+	public void setIndexList(ArrayList<Integer> indexList) {
+		this.indexList = indexList;
+	}
 
-public class Students {
-
+// addCourse, dropCourse
 }

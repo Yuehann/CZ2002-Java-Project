5bee611dd6b106091cdd5985328f2fcdab9c4d06
Yuehann <zyh990424@gmail.com>
2018-11-08 13:58:32 +0800

    user interface update

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,9 +6,19 @@ public class UserInterface {
 		
 		int choice;
 		
-		System.out.println("Please select which function to inplement:\n\n"+
-		                   "1. Register student to a course.\n"+
-		                   "2. Check vacancy available for a course index.\n");
+		System.out.println("Please select which function to implement:\n\n"+
+		                   "1.  Add a student.\n"+
+				           "2.  Add a course.\n"+
+		                   "3.  Register student for a course (include registering for Tutarial/Lab).\n"+
+		                   "4.  Check available slot in a class (vacancy in a class).\n"+
+		                   "5.  Print student list by lecture, tutorial or laboratory session for a course.\n"+
+		                   "6.  Enter course assessment components weightage.\n"+
+		                   "7.  Enter coursework mark - inclusive of its components.\n"+
+		                   "8.  Enter exam mark.\n"+
+		                   "9.  Print course statistics.\n"+
+		                   "10. Print student transcript.\n\n"+
+		                   "Enter 0 to exit the course registration system.\n");
+		System.out.println("Your choice is: ");
 		
 		
 		do {
@@ -16,7 +26,7 @@ public class UserInterface {
 		    choice = sc.nextInt();
 		
 		    switch(choice) {
-		    case 1:
+		    case 3:
 		    	System.out.printf("Student ID:");
 			    String studentId_regi = sc.next();
 			    System.out.printf("Course ID:");
@@ -29,26 +39,47 @@ public class UserInterface {
 				    System.out.printf("Registration failed, "+
 			                          "please check the information and try again.\n");
 			    break;
-			    
-		    case 2:
+         /*---------------------------------------------------------------------------------------------------- */				    
+		    case 4:
 		    	System.out.printf("Course Index:");
 			    Integer index_vacy = sc.nextInt();
 			    Integer vacancy = checkVacancyAvailable(index_vacy);
+			    if(vacancy>0)
+			    	System.out.printf("Vacancy checked!\n"+
+			                          "Vacancy available for course index "+index_vacy+" is "+vacancy+".");
+			    else
+			    	System.out.println("There currently is no vacancy available for course index "+index_vacy+".");
+			    break;
+		 /*---------------------------------------------------------------------------------------------------- */
+		    case -1:
+		    	break;
+		    default:
+		    	break;
 		    }
 		
 		}while(choice!=-1);
 
 	}
 	
-	public static Integer checkVacancyAvailable(Integer index) {
+	public static Integer checkVacancyAvailable(Integer index_vacy) {
 		
 		
 		return 0;
 	}
 	
-	public static boolean regiStuToCou(String studentId, String courseId) {
+	public static boolean regiStuToCou(String studentId_regi, String courseId_regi) {
 		
-		
+		// search to get the course object by courseId
+		// System.out.println()...all indexGroupList for this course object
+		// DO WHILE LOOP:(true?)
+		// Prompts Admin to input an index wanted
+		// checkVacancyAvailable(index) -> tell whether can register or not
+		// get the index object by searching db
+		// if (has vacancy) -> register (add record to corresponding files)
+		// else (no vacancy) -> 
+		//       prompts Admin to choose 1. choose another index to check vacancy (just keep on the do while loop?)
+		//                               2. put into waitlist for this index -> return false + words (exit loop)
+		//                               3. just exit (do not register for this course) -> return false + words (exit loop)
 		
 		return true;
 	}
